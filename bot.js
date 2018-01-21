98b2babb18fd4c93a8e35ac221f562aabf746b86
Sate2629 <Sam.Persinger@icloud.com>
2018-01-21 11:30:50 -0500

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,34 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const fs = require("fs")
+const config = require("./config.json")
+
+client.on("ready", () => {
+  console.log("I am ready!");
+
+});
+
+client.on("message", (message) => {
+
+	if(!message.content.startsWith(config.prefix)) return;
+	
+  	if (message.content.startsWith(config.prefix + "ping")) {
+    	message.channel.send("pong!");
+  	}
+  	if(message.content.startsWith(config.prefix + "prefix")){
+  		if (message.author.id !== config.ownerID) {
+  			message.channel.send("You don't have the correct permissions");
+  			return;
+  		}
+  		else {
+  		let newPrefix = message.content.split(" ").slice(1,2)[0];
+
+  		message.channel.send("prefix changed to "+ newPrefix);
+
+  		config.prefix = newPrefix;
+  		fs.writeFile("./config.json", JSON.stringify(config), (err) => console.error);
+  	}
+  	}
+});
+
+client.login(config.token);
