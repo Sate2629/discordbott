0e493ab68a8f692e204e8007b28a0cb38ba82d0c
Sate2629 <Sam.Persinger@icloud.com>
2018-01-21 12:31:18 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,6 @@ const client = new Discord.Client();
 const fs = require("fs")
 const config = require("./config.json")
 
-var a=0;
 client.on("ready", () => {
   console.log("I am ready!");
 
@@ -30,16 +29,6 @@ client.on("message", (message) => {
   		fs.writeFile("./config.json", JSON.stringify(config), (err) => console.error);
   	}
   	}
-  	if(message.content.startsWith(config.prefix + "clear")) {
-  		var a=0;
- 
-    		While (a<100){
-        		Send,{UP} {control down}a{control up} {Backspace} {Enter 2}
-        		Sleep, DELAY
-        		a++;
-    	}
-    	return
-  	}	
 });
 
 client.login(config.token);
