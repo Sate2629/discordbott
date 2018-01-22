e7d5957322720d2a9a38a86350b3f943a0cf9c1e
Sate2629 <Sam.Persinger@icloud.com>
2018-01-22 16:16:06 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -29,7 +29,7 @@ client.on("message", (message) => {
   		fs.writeFile("./config.json", JSON.stringify(config), (err) => console.error);
   	}
   	}
-	if (message.content.startsWith("?"+"help") {
+	if(message.content.startsWith("?help") {
 	    message.channel.send("The prefix is " + config.prefix);
 	}
 });
