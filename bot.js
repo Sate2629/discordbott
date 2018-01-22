13d1d13d5b44b93282a70567a91512f903fcaf7f
Sate2629 <Sam.Persinger@icloud.com>
2018-01-22 16:20:28 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,6 +11,10 @@ client.on("ready", () => {
 client.on("message", (message) => {
 
 	if(!message.content.startsWith(config.prefix)) return;
+
+  if(message.content.startsWith("?help") {
+	    message.channel.send("The prefix is " + config.prefix);
+	}
 	
   	if (message.content.startsWith(config.prefix + "ping")) {
     	message.channel.send("pong!");
