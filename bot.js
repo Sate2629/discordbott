b6c52f063fb37f73affd3ebad008e63f71197937
Sate2629 <Sam.Persinger@icloud.com>
2018-01-23 20:39:03 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -21,7 +21,7 @@ client.on("message", (message) => {
 	if(!message.content.startsWith(config.prefix)) return;
 	
 	if(message.content.startsWith(config.prefix + "roll d20")) {
-	   var dc = Math.random() * 20;
+	   var dc = Math.floor(Math.random() * 20);
 	   dc++;
 	   message.channel.send(dc);
 	   }
