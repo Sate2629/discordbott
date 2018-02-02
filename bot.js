b81b14fd42a2194ccc7a2b7d4f3ac8792555a946
sate2629 <sam.persinger@icloud.com>
2018-02-02 13:21:03 +0000

    Fixed a bug

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-const fs = require("fs")
-const config = require("./config.json")
+const fs = require("fs");
+const config = require("./config.json");
 
 client.on("ready", () => {
   console.log("I am ready!");
@@ -10,6 +10,7 @@ client.on("ready", () => {
 
 var dc = Math.floor(Math.random() *20);
 dc++;
+var num = 20;
 console.log(dc);
 
 client.on("message", (message) => {
@@ -17,14 +18,19 @@ client.on("message", (message) => {
    if(message.content.startsWith("?help")) {
 	    message.channel.send("The prefix is " + config.prefix);
 	}
+	
 
 	if(!message.content.startsWith(config.prefix)) return;
 	
-	if(message.content.startsWith(config.prefix + "roll d20")) {
-	   var dc = Math.floor(Math.random() * 20);
-	   dc++;
-	   message.channel.send(dc);
+	if(message.content.startsWith(config.prefix + "roll d"+numCh)) {
+		var dc = Math.floor(Math.random() * num);
+		dc++;
+		message.channel.send(dc);
 	   }
+	   function numCh () {
+		let num = message.content.split("  ").slice(1,2)[0];
+		return;
+	}
 	
 	if(message.content.startsWith(config.prefix + "clear")) {
 		var del = 0;
@@ -41,7 +47,7 @@ client.on("message", (message) => {
 		message.channel.send(message.content.slice(4,message.content.length));
 	}
 	
-  	if (message.content.startsWith(config.prefix + "ping")) {
+  	if (message.content.staRtsWith(config.prefix + "ping")) {
     	message.channel.send("pong!");
   	}
   	if(message.content.startsWith(config.prefix + "prefix")){
